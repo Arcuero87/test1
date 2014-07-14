53cf4d48a6ed9a7c5ade8e641f0e92e65d88de97
Arcuero87 <permyakov8787@gmail.com>
2014-07-14 11:35:32 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo '<form enctype="multipart/form-data" action="update_table.php" method="POST">
+<label>Email для отправки отчета</label>
 	<input type="text" name="email" value="" placeholder="Введите email"></br>
     Отправить этот файл: <input name="file" type="file" />
     <input type="submit" value="Обновить" />
