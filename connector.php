84494b8af620f6048e710adfa30dcdc4dc0f9f28
Arcuero87 <permyakov8787@gmail.com>
2014-07-16 10:13:48 +0600

    Create connector.php

---
+++ connector.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'localhost');      // IP адрес сервера БД
+define('DB_USERNAME', 'root');         	// Имя пользователя
+define('DB_PASSWORD', ''); 				// Пароль пользователя
+define('DB_DATABASE', 'test');        	// Имя базы данных
+define('DB_TABLE', 'test2');        	// Имя таблицы с которой работаем
